ca606a39e208aa6d62f36c8e58e8493612fc4c29
Dimitar Karailiev <mitkofff1@gmail.com>
2022-11-13 14:39:40 +0200

    getHeroById was created
    #3

--- src/hero/hero.service.ts
+++ src/hero/hero.service.ts
@@ -3,16 +3,32 @@ import { Hero } from "./hero.model";
 
 const db = {
 	marvelHeroes: [
-		{ name: "Spiderman", powers: ["net", "strong"], universe: "Marvel" },
 		{
+			id: 1,
+			name: "Spiderman",
+			powers: ["net", "strong"],
+			universe: "Marvel",
+		},
+		{
+			id: 2,
 			name: "Captain America",
 			powers: ["fly", "strong"],
 			universe: "Marvel",
 		},
 	],
 	dcHeroes: [
-		{ name: "Batman", powers: ["intelect", "skills"], universe: "DC" },
-		{ name: "Superman", powers: ["fly"], universe: "DC" },
+		{
+			id: 3,
+			name: "Batman",
+			powers: ["intelect", "skills"],
+			universe: "DC",
+		},
+		{
+			id: 4,
+			name: "Superman",
+			powers: ["fly"],
+			universe: "DC",
+		},
 	],
 };
 
@@ -23,12 +39,12 @@ export class HeroServece {
 		return heroes;
 	}
 
-	getHeroesByUniverse(universe: string): Hero[] {
-		const heroes = [...db.marvelHeroes, ...db.dcHeroes].filter(
-			(hero) => hero.universe.toLocaleLowerCase() === universe,
+	getHeroesById(id: number): Hero {
+		const hero = [...db.marvelHeroes, ...db.dcHeroes].find(
+			(hero) => hero.id === +id,
 		);
 
-		return heroes;
+		return hero;
 	}
 
 	getFilteredHeroes(filters): Hero[] {
@@ -43,8 +59,9 @@ export class HeroServece {
 							currentFilterArray.includes(element),
 						);
 				}
+				const filterValue = +filters[key] || filters[key];
 
-				return filters[key] === hero[key];
+				return filterValue === hero[key];
 			});
 		});
 
